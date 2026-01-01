08ec353b45b0b107900265a199bc89e59a6d3bf3
Hasan Omar <r3domar1@gmail.com>
2026-01-01 18:59:51 +0200

    added ex62 + extra

--- Models/Student.cs
+++ Models/Student.cs
@@ -12,7 +12,6 @@ namespace Cloud.Models
         public string ObjType { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string StudentName { get; set; }
         public double AvgGrade { get; set; }
         public string FacultyName { get; set; }
         public int SemesterNum { get; set; }
